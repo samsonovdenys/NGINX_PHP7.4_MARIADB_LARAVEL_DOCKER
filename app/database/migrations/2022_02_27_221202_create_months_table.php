efae6b70a52cfcc2a9368a22224dea06bcd0d6c0
samsonovdenys <ds126@libero.it>
2022-02-27 23:54:26 +0100

    MariaDB connected, table created

---
+++ app/database/migrations/2022_02_27_221202_create_months_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMonthsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('months', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('anno')->nullable();
+            $table->integer('mese')->nullable();
+            $table->integer('saldo')->nullable();
+            $table->integer('stipendio')->nullable();
+            $table->integer('spese')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('months');
+    }
+}
